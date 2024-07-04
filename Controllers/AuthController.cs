60942ea6b6c16ca7b3f0fcc69285311785a1d03a
islam <islam@UNKNOWN>
2024-07-04 23:09:15 +0600

    Add project files.

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using InventoryThree;
+using InventoryThree.Models;
+
+namespace InventoryThree.Controllers
+{
+    public class AuthController : Controller
+    {
+        // GET: Auth
+        public ActionResult LogIn()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult LogIn(Account model)
+        {
+            if (model.UserName == "Himel" && model.Password == "1234")
+            {
+                Session["User"] = model.UserName;
+                ViewBag.Message = "True";
+                return RedirectToAction("DashBoard", "Home");
+            }
+            else ViewBag.Message = "False";
+            return View();
+        }
+
+    }
+}
---
+++ Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using InventoryThree.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace InventoryThree.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult DashBoard()
+        {
+            Equipment.EquipmentList();
+            return View();
+        }
+    }
+}
